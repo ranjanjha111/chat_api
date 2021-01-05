3094f6ec7e5245f7b175b9bf83959aea4d90aac0
Ranjan Kumar <ranjanjha111@gmail.com>
2021-01-05 22:50:30 +0530

    Changed database url

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
